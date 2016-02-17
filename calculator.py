7569c7908837ab869dda83e9c2ba0194a56f29e2
Joshua Savon <jsavon@mail.valenciacollege.edu>
2016-02-17 02:32:02 -0500

    debug v2
    spelling error

--- calculator.py
+++ calculator.py
@@ -11,7 +11,7 @@ def subtract( first, second):
 def multiply( first, second):
     # TODO:
     # fill in code here that will return the product of first and second
-    return frist * second
+    return first * second
 
 def divide( first, second):
     # TODO:
